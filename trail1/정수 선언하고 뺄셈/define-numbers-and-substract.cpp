9414cd080c96d767c82a9a4ea2fe1ed7838f7ef1
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-15 11:36:46 +0900

    [Passed] 정수 선언하고 뺄셈 (2ms, 0MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 97;
+    int b = 13;
+
+    cout << a <<  " - " << b << " = " << a - b;
+    // Please write your code here.
+    return 0;
+}
