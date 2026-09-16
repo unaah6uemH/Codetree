9191994dc38f516c1bb02fe485a3ec2ac3e451bf
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-16 21:15:10 +0900

    [Passed] 달에서 무게 구하기 (2ms, 0MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight = 13;
+    double moonGravity = 0.165;
+
+    cout << fixed;
+
+    cout.precision(6);
+
+    cout << weight << " * " << moonGravity << " = " << weight * moonGravity;
+    // Please write your code here.
+    return 0;
+}
