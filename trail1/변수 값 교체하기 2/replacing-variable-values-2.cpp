d46df23b6b33ba48cff9c70d661d9b82566a2e9d
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-16 21:31:05 +0900

    [Passed] 변수 값 교체하기 2 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기 2/replacing-variable-values-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    a = b;
+
+    cout << a << "\n" << b;
+    // Please write your code here.
+    return 0;
+}
