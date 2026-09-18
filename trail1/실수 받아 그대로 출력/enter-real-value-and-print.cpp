856ca1e86e7300a989d6e5585ecbad7752b9fc33
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-18 19:06:01 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double N;
+
+    cin >> N;
+    
+    cout << fixed;
+    cout.precision(2);
+
+    cout << N;
+
+    // Please write your code here.
+    return 0;
+}
