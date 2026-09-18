e31781165a8886b911939e295ea952a1af2527d9
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-18 19:08:31 +0900

    [Passed] 길이 단위 환산하기 (2ms, 0MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double N;
+
+    cin >> N;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << N * 30.48;
+    // Please write your code here.
+    return 0;
+}
