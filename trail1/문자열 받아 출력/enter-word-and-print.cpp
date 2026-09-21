88d52d356afc60829a8532d5164f11269da06750
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-21 10:19:05 +0900

    [Passed] 문자열 받아 출력 (2ms, 0MB)

---
+++ trail1/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string S;
+
+    cin >> S;
+
+    cout << S;
+    // Please write your code here.
+    return 0;
+}
