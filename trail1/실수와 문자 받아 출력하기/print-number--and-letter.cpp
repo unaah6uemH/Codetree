c9e30aad13b2e1df046f3e942a0c275607d35ae6
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-21 10:20:53 +0900

    [Passed] 실수와 문자 받아 출력하기 (2ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+
+    cin >> c >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+    
+    cout << c << "\n" << a << "\n" << b;
+    // Please write your code here.
+    return 0;
+}
