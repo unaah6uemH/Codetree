7ad8aa00d4f3a616948e7d2cecf9f2eb2d9d04f1
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-22 09:39:16 +0900

    [Passed] 세 수의 합과 평균 구하기 (2ms, 0MB)

---
+++ trail1/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c << "\n";
+    cout << (a + b + c) / 3;
+    // Please write your code here.
+    return 0;
+}
