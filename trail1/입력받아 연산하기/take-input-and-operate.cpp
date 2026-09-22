bb13915c603e383bef138534e1dce0aa664999c0
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-22 09:27:26 +0900

    [Passed] 입력받아 연산하기 (2ms, 0MB)

---
+++ trail1/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + 87 << "\n";
+    cout << b % 10;
+    // Please write your code here.
+    return 0;
+}
