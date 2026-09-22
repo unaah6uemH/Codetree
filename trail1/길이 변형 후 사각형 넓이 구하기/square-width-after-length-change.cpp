d4acee409072be3a43e7558c8a57616d23554a74
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-22 09:23:58 +0900

    [Passed] 길이 변형 후 사각형 넓이 구하기 (2ms, 0MB)

---
+++ trail1/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w, h;
+
+    cin >> w >> h;
+
+    w += 8;
+    h *= 3;
+
+    cout << w << "\n" << h << "\n" << w * h;
+    // Please write your code here.
+    return 0;
+}
