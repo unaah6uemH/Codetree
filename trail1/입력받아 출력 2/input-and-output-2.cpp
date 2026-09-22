b0bac94b93c16dd3677fbbe6a5d150f56050f215
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-22 09:07:38 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int f, b;
+
+    cin >> f;
+    cin.get();
+    cin >> b;
+
+    cout << f << b;
+    // Please write your code here.
+    return 0;
+}
