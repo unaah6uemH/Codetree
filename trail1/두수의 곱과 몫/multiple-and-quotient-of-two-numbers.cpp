3783f7ea20d5674b5b8eb9b6569e6d58dfd33e62
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-22 09:25:30 +0900

    [Passed] 두수의 곱과 몫 (1ms, 0MB)

---
+++ trail1/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a << " * " << b << " = " << a * b << "\n";
+    cout << a << " / " << b << " = " << a / b;
+    // Please write your code here.
+    return 0;
+}
