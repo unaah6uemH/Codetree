a800eb45ed3b68886d05bf5126f30491c0e997a3
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-22 09:16:27 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int x, y;
+    int z;
+
+    cin >> z;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+
+    cout << "010" << "-" << y << "-" << x;
+    // Please write your code here.
+    return 0;
+}
