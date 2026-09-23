24ad30ab05d87c844e8226e18c5b5ad2cc494d15
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-24 00:28:45 +0900

    [Passed] 출석 부르기 (1ms, 0MB)

---
+++ trail1/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cin >> num;
+
+    if (num == 1)
+        cout << "John";
+    else if (num == 2)
+        cout << "Tom";
+    else if (num == 3)
+        cout << "Paul";
+    else
+        cout << "Vacancy";
+    // Please write your code here.
+    return 0;
+}
