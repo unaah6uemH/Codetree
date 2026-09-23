b5ad92f3f3cca7209b50c8b736ac4df3a773c149
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-24 00:05:28 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ trail1/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    cout << n << "\n";
+
+    if (n < 0) {
+        cout << "minus";
+    }
+    // Please write your code here.
+    return 0;
+}
