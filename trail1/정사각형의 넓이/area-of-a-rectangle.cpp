e73e8af2f95e8432c40f0aae43fe9911ad8be6a3
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-24 00:11:57 +0900

    [Passed] 정사각형의 넓이 (2ms, 0MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    cout << n * n << "\n";
+
+    if (n < 5) {
+        cout << "tiny";
+    }
+    // Please write your code here.
+    return 0;
+}
