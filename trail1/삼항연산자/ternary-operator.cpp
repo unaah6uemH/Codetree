ec44fd9ca16fcbb705c4d7101233d91eeeb57206
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-24 00:20:36 +0900

    [Passed] 삼항연산자 (2ms, 0MB)

---
+++ trail1/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+
+    cin >> score;
+
+    score == 100 ? cout << "pass" : cout << "failure";
+    // Please write your code here.
+    return 0;
+}
