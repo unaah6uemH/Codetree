e4be4538080bff25f374cb2285ee2d588ea34e4d
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-24 00:14:11 +0900

    [Passed] 정수의 조건 여부 (2ms, 0MB)

---
+++ trail1/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if (a >= 113) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    // Please write your code here.
+    return 0;
+}
