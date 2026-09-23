0c008e43f00b5b11d2bc43d81bb268744c205cad
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-24 00:17:17 +0900

    [Passed] 2개 중 최대 (2ms, 0MB)

---
+++ trail1/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b;
+
+    c = a > b ? a : b;
+
+    cout << c;
+    // Please write your code here.
+    return 0;
+}
