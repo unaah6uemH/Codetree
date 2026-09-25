0c1bf68e111b645f7562a88c19c0e7240fcde4fc
unaah6uemH <o1xe2vn@yonsei.ac.kr>
2026-09-25 23:47:50 +0900

    [Passed] 최대 2번의 연산 (1ms, 0MB)

---
+++ trail1/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if (a % 2 == 0) {
+        a /= 2;
+    }
+
+    if (a % 2 != 0) {
+        a = (a + 1) / 2;
+    }
+
+    cout << a;
+    // Please write your code here.
+    return 0;
+}
